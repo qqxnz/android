3529e35c09409272bb31c27cd6ba2f3fe58a83dd
扶美(吕扶美) <fumei@enai.im>
2022-01-26 12:59:51 +0800

    add static list

--- list/app/src/main/java/com/qqxnz/list/MainActivity.java
+++ list/app/src/main/java/com/qqxnz/list/MainActivity.java
@@ -3,12 +3,27 @@ package com.qqxnz.list;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ListView listView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        String[] strings = new String[]{"全部","电影","图书","游戏","电视"};
+
+        listView = findViewById(R.id.listView);
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,strings);
+//
+//        listView.setAdapter(adapter);
+
     }
 }
